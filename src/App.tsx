28f70c14cb686cdc25d585a850d01ed868a0827f
laurenCho9 <lauren.choux@gmail.com>
2024-08-07 22:58:16 +0900

    feat: 상태관리와 Props 2 (useState로 관리 및 파일 분할)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
-
-interface Todo {
-  id: number;
-  content: string;
-}
+import { Todo } from "./types";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
@@ -16,6 +13,10 @@ function App() {
     setTodos([...todos, { id: idRef.current++, content: text }]);
   };
 
+  const onClickDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   useEffect(() => {
     console.log(todos);
   }, [todos]);
@@ -24,6 +25,11 @@ function App() {
     <div className="App">
       <h1>Todo</h1>
       <Editor onClickAdd={onClickAdd} />
+      <div>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} onClickDelete={onClickDelete} />
+        ))}
+      </div>
     </div>
   );
 }
